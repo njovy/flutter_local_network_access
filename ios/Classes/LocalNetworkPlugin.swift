fab7af4ce64a9f5c05d47a15372470f5ba0efd9e
Taehyun Park <root@njovy.com>
2023-09-11 02:07:09 +0900

    feat: Return true for local network access on iOS < 14
    Adjusted the local_network plugin to automatically grant local network access for devices running iOS versions below 14, as the Local Network Access permission is introduced in iOS 14. For iOS 14 and above, the plugin continues to check the actual permission status.

--- ios/Classes/LocalNetworkPlugin.swift
+++ ios/Classes/LocalNetworkPlugin.swift
@@ -3,23 +3,27 @@ import UIKit
 
 public class LocalNetworkPlugin: NSObject, FlutterPlugin {
     let localNetworkPrivacy = LocalNetworkPrivacy()
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "com.njovy.libs/local_network", binaryMessenger: registrar.messenger())
-    let instance = LocalNetworkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-     case "checkLocalNetworkAccess":
-            localNetworkPrivacy.checkAccessState { granted in
-                result(granted)
-            }
-    default:
-      result(FlutterMethodNotImplemented)
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.njovy.libs/local_network", binaryMessenger: registrar.messenger())
+        let instance = LocalNetworkPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "checkLocalNetworkAccess":
+            if #available(iOS 14, *) {
+                localNetworkPrivacy.checkAccessState { granted in
+                    result(granted)
+                }
+            } else {
+                result(true)
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
